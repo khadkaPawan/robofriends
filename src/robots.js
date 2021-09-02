ebd50e504c2ca751aedfadf4abe7ec13a3feaad6
khadkaPawan <pawan@ismt.edu.np>
2021-09-02 11:10:21 +0545

    adding first and final project.

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+    {
+        id:1,
+        name:'leane Grendy',
+        username:'Bret',
+        email:'lean@gamil.com'
+    },
+    {
+        id:2,
+        name:'Humpak Hill',
+        username:'Hull',
+        email:'humpak@gamil.com'
+    },
+    {
+        id:3,
+        name:'Brian Getto',
+        username:'Virdit',
+        email:'virdict@gamil.com'
+    },
+    {
+        id:4,
+        name:'Jenifor Lokki',
+        username:'Lillcas',
+        email:'jenloki@gamil.com'
+    },
+    {
+        id:5,
+        name:'Giggy Stwort',
+        username:'Hadid',
+        email:'Uphadid@gamil.com'
+    },
+    {
+        id:6,
+        name:'Narina Joseh',
+        username:'Steric',
+        email:'sterinc@gamil.com'
+    },
+    {
+        id:7,
+        name:'Ford Raptor',
+        username:'Ammic',
+        email:'amminc@gamil.com'
+    },
+    {
+        id:8,
+        name:'Cindrella Jossy',
+        username:'Jess',
+        email:'cinkoss@gamil.com'
+    },
+    {
+        id:9,
+        name:'Amidon Liber',
+        username:'Solic',
+        email:'solicam@gamil.com'
+    },
+    {
+        id:10,
+        name:'Youg Lou',
+        username:'limma',
+        email:'loulima@gamil.com'
+    },
+]
